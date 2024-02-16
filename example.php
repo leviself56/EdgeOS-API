547427b2ab499e9f84eb6a311cca216cfab77d16
leviself56 <160255898+leviself56@users.noreply.github.com>
2024-02-16 10:15:03 -0600

    Update example.php

--- example.php
+++ example.php
@@ -1,10 +1,12 @@
 <?php
 require_once("class.edgeos.php");
 
-$EdgeOS = new EdgeOS($ip="10.15.100.170", $user="ubnt", $pass="ubnt");
+$EdgeOS = new EdgeOS($ip="10.15.100.170", $user="ubnt", $pass="Get214Out");
 
+echo "<pre>";
 print_r($EdgeOS->GetSFPs());
 print_r($EdgeOS->GetInterfaces());
 print_r($EdgeOS->GetSystemInfo());
+echo "</pre>";
 
 ?>
